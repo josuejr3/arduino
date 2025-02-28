d5695a3256ba6a3c7402d1eb1d22541362cf70a4
Josue <geenujunior@gmail.com>
2025-02-28 17:11:58 -0300

    exemplo de código para um semáforo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
+// Criando um semáforo com Arduino
+
 #include <Arduino.h>
+
+#define RED_LED 8
+#define YELLOW_LED 9
+#define GREEN_LED 10
+
+
 void setup() {
-// write your initialization code here
+    pinMode(RED_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
 }
 
+void red(int tmp){
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    delay(tmp  * 1000);
+}
+
+void yellow(int tmp){
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    delay(tmp * 1000);
+}
+
+void green(int tmp){
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    delay(tmp * 1000);
+}
+
+
 void loop() {
-// write your code here
+    red(7);
+    green(9);
+    yellow(2);
 }
